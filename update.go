6dcfc1c78bd82ff8bf1a00dca1e5d52c9d4ad8a3
Melchizedek Shah <melchizedekshah18@proton.me>
2025-12-22 13:17:03 -0400

    Update update.go, fix for to get acces to the version of Nextstep. check the web app project to see commit. To avoid rate limiting problems in the future

--- update.go
+++ update.go
@@ -8,26 +8,21 @@ import (
 	"os"
 )
 
-// Online
-type nextstepJsonApi struct {
-	Tag_name    string `json:"tag_name"`
-	Tarball_url string `json:"tarball_url"`
+// For online json
+type packageOnlineJson struct {
+	Version       string `json:"version"`
+	Url           string `json:"download_url"`
+	Release_notes string `json:"release_notes"`
 }
 
-// Local
-type nextstepJsonLocal struct {
+// For local json
+type packageLocalJson struct {
 	NextStep nextStep `json:"nextstep"`
 }
-
 type nextStep struct {
 	Name    string `json:"name"`
 	Version string `json:"version"`
-	API     api    `json:"api"`
-}
-
-type api struct {
-	Releases string `json:"releases"`
-	Repo     string `json:"repo"`
+	Remote  string `json:"remote_project"`
 }
 
 func UpdateNextStep() {
@@ -35,9 +30,30 @@ func UpdateNextStep() {
 }
 
 func versionchecker() {
-	// Get version on github
-	url := "https://api.github.com/repos/NextStepWebApp/NextStep/releases/latest"
-	response, err := http.Get(url)
+	// Get local version
+	jsonLocalFile, err := os.Open("/home/william/Documents/programming/PWS/nstep/package.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer jsonLocalFile.Close()
+
+	packageLocalItem := packageLocalJson{}
+	decoder := json.NewDecoder(jsonLocalFile)
+	decoder.DisallowUnknownFields()
+
+	if err := decoder.Decode(&packageLocalItem); err != nil {
+		log.Fatal("Decode error: ", err)
+	}
+
+	localVersion := packageLocalItem.NextStep.Version
+	fmt.Println(localVersion)
+
+	// Get the url to see the verion of the project
+	remotePackageUrl := packageLocalItem.NextStep.Remote
+	fmt.Println(remotePackageUrl)
+
+	response, err := http.Get(remotePackageUrl)
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -47,33 +63,13 @@ func versionchecker() {
 		log.Fatal(err)
 	}
 
-	nextstepJsonApiItem := nextstepJsonApi{}
-	decoder := json.NewDecoder(response.Body)
+	packageOnlineItem := packageOnlineJson{}
+	decoder = json.NewDecoder(response.Body)
 
-	if err := decoder.Decode(&nextstepJsonApiItem); err != nil {
+	if err := decoder.Decode(&packageOnlineItem); err != nil {
 		log.Fatal("Decode error: ", err)
 	}
 
-	onlineVersion := nextstepJsonApiItem.Tag_name
+	onlineVersion := packageOnlineItem.Version
 	fmt.Println(onlineVersion)
-
-	// Get local version
-
-	jsonLocalFile, err := os.Open("/home/william/Documents/programming/PWS/nstep/package.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer jsonLocalFile.Close()
-
-	nextstepJsonLocalItem := nextstepJsonLocal{}
-
-	decoder = json.NewDecoder(jsonLocalFile)
-
-	if err := decoder.Decode(&nextstepJsonLocalItem); err != nil {
-		log.Fatal("Decode error: ", err)
-	}
-
-	localVersion := nextstepJsonLocalItem.NextStep.Version
-	fmt.Println(localVersion)
-
 }
