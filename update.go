877106e3d79cbb4d00778812ef052641e737b19b
Melchizedek Shah <melchizedekshah18@proton.me>
2025-12-22 21:18:55 -0400

    Update update.go

--- update.go
+++ update.go
@@ -3,73 +3,114 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 )
 
 // For online json
 type packageOnlineJson struct {
-	Version       string `json:"version"`
-	Url           string `json:"download_url"`
-	Release_notes string `json:"release_notes"`
+	Version      string `json:"version"`
+	Url          string `json:"download_url"`
+	ReleaseNotes string `json:"release_notes"`
 }
 
 // For local json
 type packageLocalJson struct {
 	NextStep nextStep `json:"nextstep"`
 }
+
 type nextStep struct {
 	Name    string `json:"name"`
 	Version string `json:"version"`
 	Remote  string `json:"remote_project"`
 }
 
-func UpdateNextStep() {
-	versionchecker()
+// To store information for version checker
+type versionCheck struct {
+	CurrentVersion  string
+	LatestVersion   string
+	UpdateAvailable bool
+	Message         string
+	DownloadURL     string
+	ReleaseNotes    string
 }
 
-func versionchecker() {
-	// Get local version
-	jsonLocalFile, err := os.Open("/home/william/Documents/programming/PWS/nstep/package.json")
+func UpdateNextStep() {
+	result, err := versionchecker()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("Error checking version: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(result.Message)
+	if result.UpdateAvailable {
+		fmt.Printf("New version available: %s\n", result.LatestVersion)
+		fmt.Printf("Download: %s\n", result.DownloadURL)
+		fmt.Printf("Release notes: %s\n", result.ReleaseNotes)
+	}
+	os.Exit(0)
+}
+
+// This function gets the local version and remote project version
+// And then compares them to see if a new version came out
+func versionchecker() (*versionCheck, error) {
+	// Get local version
+
+	configpath, err := Getpackagedir("/home/william/Documents/programming/PWS/nstep/config.json")
+	if err != nil {
+		return nil, fmt.Errorf("cannot open config: %w", err)
+	}
+
+	jsonLocalFile, err := os.Open(configpath)
+	if err != nil {
+		return nil, fmt.Errorf("cannot open package.json: %w", err)
 	}
 	defer jsonLocalFile.Close()
 
 	packageLocalItem := packageLocalJson{}
 	decoder := json.NewDecoder(jsonLocalFile)
 	decoder.DisallowUnknownFields()
-
 	if err := decoder.Decode(&packageLocalItem); err != nil {
-		log.Fatal("Decode error: ", err)
+		return nil, fmt.Errorf("cannot decode local package.json: %w", err)
 	}
 
-	localVersion := packageLocalItem.NextStep.Version
-	fmt.Println(localVersion)
-
-	// Get the url to see the verion of the project
+	// Get the url to see the version of the project
 	remotePackageUrl := packageLocalItem.NextStep.Remote
-	fmt.Println(remotePackageUrl)
-
 	response, err := http.Get(remotePackageUrl)
-
 	if err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("cannot fetch remote version: %w", err)
 	}
 	defer response.Body.Close()
 
 	if response.StatusCode != http.StatusOK {
-		log.Fatal(err)
+		return nil, fmt.Errorf("remote server returned status: %d", response.StatusCode)
 	}
 
 	packageOnlineItem := packageOnlineJson{}
 	decoder = json.NewDecoder(response.Body)
-
 	if err := decoder.Decode(&packageOnlineItem); err != nil {
-		log.Fatal("Decode error: ", err)
+		return nil, fmt.Errorf("cannot decode remote package.json: %w", err)
 	}
 
+	localVersion := packageLocalItem.NextStep.Version
 	onlineVersion := packageOnlineItem.Version
-	fmt.Println(onlineVersion)
+
+	// Create result struct
+	result := &versionCheck{
+		CurrentVersion: localVersion,
+		LatestVersion:  onlineVersion,
+		DownloadURL:    packageOnlineItem.Url,
+		ReleaseNotes:   packageOnlineItem.ReleaseNotes,
+	}
+
+	// Compare the versions to see if an update is needed
+	if localVersion == onlineVersion {
+		result.UpdateAvailable = false
+		result.Message = fmt.Sprintf("Already up to date (%s)", localVersion)
+	} else {
+		result.UpdateAvailable = true
+		result.Message = fmt.Sprintf("Update available: %s -> %s", localVersion, onlineVersion)
+	}
+
+	return result, nil
 }
