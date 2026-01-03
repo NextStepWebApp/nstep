b85b90be65459dff0e850889cb5c86eaffcad445
Melchizedek Shah <melchizedekshah18@proton.me>
2026-01-03 20:39:34 +0200

    Update install.go

--- install.go
+++ install.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 )
 
@@ -10,6 +11,8 @@ func InstallNextStep(plj *packageLocalJson) error {
 	installScript := plj.GetNextStepInstallScript()
 
 	cmd := exec.Command("bash", installScript)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("error executing nextstep install script %w\n", err)
