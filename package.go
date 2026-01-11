8573d8d0eec1d131dcc83af991019cdbf660d6b0
Melchizedek Shah <melchizedekshah18@proton.me>
2026-01-10 20:35:56 -0400

    Update package.go

--- package.go
+++ package.go
@@ -198,6 +198,13 @@ func NextStepSetup(cfg config, resultversion *versionCheck, plj *packageLocalJso
 
 	}
 
+	// get the current version to the web portal
+
+	cmd := exec.Command("cp", "-r", currentfilepath, plj.GetLocalWebpath())
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("cannot copy to web portal: %w", err)
+	}
+
 	// Move all the files to there places
 	moves := [][2]string{
 		{"/srv/http/NextStep/config/nextstep_config.json", "/etc/nextstepwebapp/nextstep_config.json"},
