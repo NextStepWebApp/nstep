0cfba320d8b4ea766a790667ec7829590de5ed55
Melchizedek Shah <melchizedekshah18@proton.me>
2026-01-22 21:52:15 -0400

    Create setup.go

---
+++ setup.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+// struct for the setup.json
+type setup struct {
+	ProjectName string `json:"project_name"`
+	VersionUrl  string `json:"version_url"`
+}
+
+// This function gets the version url of the remote project
+// At the beginning that does not exits so get it from that
+// and after delete the install.json (no longer needed)
+func setupLocalPackage(cfg config) error {
+	var err error
+
+	setupFile, err := os.Open(cfg.getSetupFile())
+	if err != nil {
+		return fmt.Errorf("cannot open setup file %w", err)
+	}
+	defer setupFile.Close()
+
+	// Get the information
+	setupItem := setup{}
+	decoder := json.NewDecoder(setupFile)
+	decoder.DisallowUnknownFields()
+
+	if err := decoder.Decode(&setupItem); err != nil {
+		return fmt.Errorf("cannot decode %w", err)
+	}
+
+	projectName := setupItem.ProjectName
+	projectUrl := setupItem.VersionUrl
+
+	fmt.Println("===> Package build setup...")
+	fmt.Printf("Downloading %s package.json from %s\n", projectName, projectUrl)
+
+	message, err := downloadpackage(projectUrl, cfg.getPackagePath())
+	if err != nil {
+		return fmt.Errorf("%w", err)
+	}
+	fmt.Println(message)
+
+	fmt.Println("===> Package build setup completed successfully")
+	return nil
+}
